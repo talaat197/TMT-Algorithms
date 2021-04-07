a3cb5dbb6d1671d38ce8f2788ecdc7f6f82d68f1
Talaat <talaatmedhat19@gmail.com>
2021-04-07 12:13:05 +0200

    breadth first search algorithm

---
+++ breadthFirstSearch.js
@@ -0,0 +1,29 @@
+/**
+ * Time complexity O(V + E)
+ * V is the number of nodes
+ * E is the number of adjacents
+ *
+ * space complexity is O(V)
+ */
+
+function breadthFirstSearch(graph, rootNode) {
+  let queue = [], visited = [];
+  queue[0] = rootNode;
+
+  while (queue.length) {
+    let visitedNode = queue.shift();
+
+    console.log(visitedNode, queue);
+
+    graph[visitedNode].forEach((neighbour) => {
+      if (!visited.includes(neighbour)) {
+        visited.push(neighbour);
+        queue.push(neighbour);
+      }
+    });
+  }
+}
+
+const graph = { 0: [1, 2], 1: [2], 2: [3], 3: [1, 2] };
+
+breadthFirstSearch(graph, 0);
