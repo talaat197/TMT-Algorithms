2358e56ec83bed4734aebe642030e612312d342a
Talaat Medhat <talaat.medhat@musala.com>
2022-04-16 23:01:10 +0200

    refactor(binary-search): enhance binary search code style

--- searching/binary_search.js
+++ searching/binary_search.js
@@ -8,7 +8,7 @@ function binarySearch(arr, searchValue) {
   const arrayLength = arr.length;
   let lowIndex = 0;
   let highIndex = arrayLength - 1;
-  let mid = (lowIndex + highIndex) / 2;
+  let mid = parseInt((lowIndex + highIndex) / 2);
 
   while (lowIndex <= highIndex) {
     if (searchValue === arr[mid]) return mid;
@@ -20,7 +20,7 @@ function binarySearch(arr, searchValue) {
       highIndex = mid - 1;
     }
 
-    mid = (lowIndex + highIndex) / 2;
+    mid = parseInt((lowIndex + highIndex) / 2);
   }
   // not found
   return -1;
@@ -29,21 +29,22 @@ function binarySearch(arr, searchValue) {
 function recursionBinarySearch(arr, searchValue, lowIndex, highIndex) {
   // not found
   if (lowIndex > highIndex) return -1;
-  else {
-    mid = (lowIndex + highIndex) / 2;
 
-    if (arr[mid] === searchValue) return mid;
-    else if (searchValue > arr[mid]) {
-      lowIndex = mid + 1;
-      return binarySearch(arr, searchValue, lowIndex, highIndex);
-    } else if (searchValue < arr[mid]) {
-      highIndex = mid - 1;
-      return binarySearch(arr, searchValue, lowIndex, highIndex);
-    }
+  mid = parseInt((lowIndex + highIndex) / 2);
+
+  if (arr[mid] === searchValue) return mid;
+  
+  if (searchValue > arr[mid]) {
+    lowIndex = mid + 1;
+    return binarySearch(arr, searchValue, lowIndex, highIndex);
+  }
+    if (searchValue < arr[mid]) {
+    highIndex = mid - 1;
+    return binarySearch(arr, searchValue, lowIndex, highIndex);
   }
 }
 
-let testArray = [1, 2, 3, 4, 5, 6, 7];
+let testArray = [1, 2, 3, 4, 5, 6, 7,2];
 
 console.log(binarySearch(testArray, 2));
 
